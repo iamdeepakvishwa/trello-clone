e699c238f18e0983fff6513ea3df26f751c6b48a
iamdeepakvishwa <kumar110898@gmail.com>
2020-09-30 22:40:40 +0530

    :tada:

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -6,6 +6,7 @@
                 <v-form
                     ref="form"
                     v-model="valid"
+                    @submit.prevent = "signUp"
                     lazy-validation
                 >
                     <v-text-field
@@ -17,7 +18,7 @@
 
                     <v-text-field
                         v-model="user.displayName"
-                        :rules="userNameRules"
+                        :rules="nameRules"
                         label="Name"
                         required
                     ></v-text-field>
@@ -27,6 +28,7 @@
                         :rules="passwordRules"
                         label="Password"
                         type="password"
+                        autocomplete="off"
                         required
                     ></v-text-field>
 
@@ -34,16 +36,18 @@
                         v-model="user.confirmPassword"
                         :rules="passwordRules"
                         label="Confirm Password"
+                        autocomplete="off"
                         type="password"
                         required
                     ></v-text-field>
 
                     <v-text-field
                         v-model="user.imageUrl"
-                        :rules="imageUrlRules"
+                        :rules="notEmptyRules"
                         label="Image Url"
                         required
                     ></v-text-field>
+                    <v-btn type= "submit" >SignUp</v-btn>
                 </v-form>
             </v-col>
         </v-row>
@@ -55,15 +59,37 @@
 /* eslint-disable */
 export default {
     name: 'SignUp',
-    data : ()=>({
+    data : (vm)=>({
+        valid: false,
         user: {
             username: '',
             displayName: '',
             password: '',
             confirmPassword: '',
             imageUrl: '',
-        }
+        },
+        notEmptyRules: [
+            v => !!v || 'This field cannot be empty'
+        ],
+        nameRules: [
+            v => !!v || 'Name is required',
+            v => (v && v.length <= 20) || 'Name must be less than 20 characters',
+        ],
+        userNameRules: [
+            v => !!v || 'Username is required',
+            v => /^[a-zA-Z\_.]+$/.test(v) || 'UserName is Not valid ',
+            v => v.length > 5 || 'UserName must be atleast 5 characters',
+        ],
+        passwordRules: [
+            (confirmPassword) => confirmPassword === vm.user.password || 'Password must match'
+        ]
+
     }),
+    methods: {
+        signUp(){
+
+        },
+    }
 }
 </script>
 
